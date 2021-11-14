31ecf2d7e50ae980826f41df360956f0e5f5c126
kro01 <k.e.dimitrov@gmail.com>
2021-11-14 02:14:42 +0200

    !

--- src/employee/Main.java
+++ src/employee/Main.java
@@ -35,10 +35,6 @@ public class Main{
         try(BufferedReader br = new BufferedReader(new FileReader(file))) {
     		for(String line; (line = br.readLine()) != null; ) {
         		String[] str = line.replaceAll(" ","").split(",",5);
-        		/*System.out.println(str.length);
-        		for(int i=0; i<str.length; i++){
-        			System.out.println(str[i]);
-				}*/
 				int emplID = Integer.valueOf(str[0]);
 				if(emp.containsKey(emplID)){
 					emp.get(emplID).add(new Task(str));
@@ -74,7 +70,6 @@ public class Main{
 				keyE2 = e2.next();
 				emp1 = emp.get(keyE1);
 				emp2 = emp.get(keyE2);
-				System.out.printf("\n%d %d \n",keyE1,keyE2);
 				for(int i = 0; i < emp1.size(); i++){
 					for(int j = 0; j  < emp2.size(); j++){
 						int i2 = -1;
@@ -83,12 +78,8 @@ public class Main{
 							for(i2 = i; i2 < emp1.size() && emp1.get(i2).getProjectID() == emp2.get(j).getProjectID();i2++ ){
 								for(j2=j; j2<emp2.size() && emp1.get(i2).getProjectID() == emp2.get(j2).getProjectID();j2++ ){
 									if( emp1.get(i2).getDateFrom().isBefore(emp2.get(j2).getDateTo()) && emp1.get(i2).getDateTo().isAfter(emp2.get(j2).getDateFrom()) ){
-										//print Task1 Task2
-										//System.out.printf("%d %d %s %s\n",emp1.get(i2).getEmpID(),emp1.get(i2).getProjectID(),emp1.get(i2).getDateFrom().toString(),emp1.get(i2).getDateTo().toString());
-										//System.out.printf("%d %d %s %s\n\n",emp2.get(j2).getEmpID(),emp2.get(j2).getProjectID(),emp2.get(j2).getDateFrom().toString(),emp2.get(j2).getDateTo().toString());
 										start = emp1.get(i2).getDateFrom().isAfter(emp2.get(j2).getDateFrom()) ? emp1.get(i2).getDateFrom() : emp2.get(j2).getDateFrom();
 										end = emp1.get(i2).getDateTo().isBefore(emp2.get(j2).getDateTo()) ? emp1.get(i2).getDateTo() : emp2.get(j2).getDateTo();
-										//System.out.printf("%d %d %d\n",Period.between(start, end).getYears(),Period.between(start, end).getMonths(),Period.between(start, end).getDays());
 										if(!results.containsKey(keyE1)){
 											results.put(keyE1,new HashMap<Integer,Period>());
 											results.get(keyE1).put(keyE2,Period.between(start, end));
@@ -97,7 +88,6 @@ public class Main{
 										else
 											results.get(keyE1).put(keyE2,results.get(keyE1).get(keyE2).plus(Period.between(start, end)));
 										maxCopy = max.minus(results.get(keyE1).get(keyE2));
-										//System.out.printf("%d %d %d\n",results.get(keyE1).get(keyE2).getYears(),results.get(keyE1).get(keyE2).getMonths(),results.get(keyE1).get(keyE2).getDays());
 										if(maxCopy.isNegative()){
 											maxCopy = results.get(keyE1).get(keyE2);
 											max = Period.of(maxCopy.getYears(),maxCopy.getMonths(),maxCopy.getDays());
